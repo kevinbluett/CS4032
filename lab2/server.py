a6f2a9dd6dd48a7e78d0d02c404d12e24e9ff43e
Kevin Bluett <bluettk@tcd.ie>
2014-10-23 15:48:38 +0100

    Turns out IP = full hostname

--- lab2/server.py
+++ lab2/server.py
@@ -11,7 +11,7 @@ def lab2_handler(server, (sock, addr)):
         if server is not None:
             Thread(target=server.shutdown, args=[False]).start()
     else:
-        sock.send("%sIP:%s\nPort:%s\nStudentID:11311101" % (data, socket.gethostbyname(socket.gethostname()), sys.argv[1]))
+        sock.send("%sIP:%s\nPort:%s\nStudentID:11311101" % (data,  socket.gethostbyaddr(socket.gethostbyname(socket.gethostname()))[0], sys.argv[1]))
 
     # Kill the socket
     sock.close()
